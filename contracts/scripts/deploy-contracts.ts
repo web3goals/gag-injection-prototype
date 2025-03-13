f128f07d11fe1d7bc5dd225bbd8843da4fe03b70
kiv1n <aloha@kiv1n.ru>
2025-03-13 12:42:06 +0300

    Refactor script to deploy contracts

--- contracts/scripts/deploy-contracts.ts
+++ contracts/scripts/deploy-contracts.ts
@@ -4,48 +4,47 @@ import TokenFactory from "../artifacts/contracts/TokenFactory.sol/TokenFactory.j
 import { injectiveTestnet } from "./data/chains";
 import { CONTRACTS } from "./data/deployed-contracts";
 
+async function deployContracts() {
+  const network = hre.network.name;
+  const [deployer] = await hre.viem.getWalletClients();
+
+  if (!CONTRACTS[network].tokenFactory) {
+    const contract = await hre.viem.deployContract("TokenFactory", [
+      deployer.account.address,
+    ]);
+    console.log(`Contract 'TokenFactory' deployed to: ${contract.address}`);
+  }
+}
+
+async function deployInjectiveTestnetContracts() {
+  const network = hre.network.name;
+  const publicClient = await hre.viem.getPublicClient({
+    chain: injectiveTestnet,
+  });
+  const [deployer] = await hre.viem.getWalletClients({
+    chain: injectiveTestnet,
+  });
+
+  if (!CONTRACTS[network].tokenFactory) {
+    const hash = await deployer.deployContract({
+      abi: TokenFactory.abi,
+      bytecode: TokenFactory.bytecode as Hex,
+      args: [deployer.account.address],
+    });
+    const receipt = await publicClient.waitForTransactionReceipt({ hash });
+    console.log(
+      `Contract 'TokenFactory' deployed to: ${receipt.contractAddress}`
+    );
+  }
+}
+
 async function main() {
   console.log("👟 Start script 'deploy-contracts'");
 
-  const network = hre.network.name;
-
-  // Deploy token factory contract
-  if (!CONTRACTS[network].tokenFactory) {
-    // Deploy contract on Injective Testnet
-    let contractAddress = undefined;
-    if (network === "injectiveTestnet") {
-      console.log("Here");
-      const [deployer] = await hre.viem.getWalletClients({
-        chain: injectiveTestnet,
-      });
-      const walletClients = await hre.viem.getWalletClients({
-        chain: injectiveTestnet,
-      });
-      const walletClient = walletClients[0];
-      if (!walletClient) {
-        throw new Error("No wallet client found");
-      }
-      const hash = await walletClient.deployContract({
-        abi: TokenFactory.abi,
-        bytecode: TokenFactory.bytecode as Hex,
-        args: [deployer.account.address],
-      });
-      const publicClient = await hre.viem.getPublicClient({
-        chain: injectiveTestnet,
-      });
-      const receipt = await publicClient.waitForTransactionReceipt({ hash });
-      contractAddress = receipt.contractAddress;
-    }
-    // Deploy contract on other networks
-    else {
-      const [deployer] = await hre.viem.getWalletClients();
-      const contract = await hre.viem.deployContract("TokenFactory", [
-        deployer.account.address,
-      ]);
-      contractAddress = contract.address;
-    }
-
-    console.log(`Contract 'TokenFactory' deployed to: ${contractAddress}`);
+  if (hre.network.name === "injectiveTestnet") {
+    deployInjectiveTestnetContracts();
+  } else {
+    deployContracts();
   }
 }
 
